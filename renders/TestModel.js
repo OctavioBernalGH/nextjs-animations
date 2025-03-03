6b6003e21aacae6ae66c2b468871b2a929a3aabe
Octavio Bernal Vilana <103035621+OctavioBernalGH@users.noreply.github.com>
2025-03-03 15:57:03 +0100

    Modificaciones

--- renders/TestModel.js
+++ renders/TestModel.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas, useFrame } from "@react-three/fiber";
-import { OrbitControls, useGLTF, Html } from "@react-three/drei"; // 🔹 Importamos `Html`
+import { OrbitControls, useGLTF, Html } from "@react-three/drei";
 import { Suspense, useRef, useState, useEffect } from "react";
 
 function Model({ setLoaded }) {
